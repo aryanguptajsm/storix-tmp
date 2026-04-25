d2875def720889dbefe6ba6cdc1b60a88fd61bed
Aryanguptajsm <aryangupta.jsm@gmail.com>
2026-04-26 00:00:08 +0530

    refactor: remove unused chat widget components and knowledge base configuration

--- src/components/ui/ClientChatWidget.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-export const ClientChatWidget = dynamic(() => import("@/components/ui/ChatWidget").then(m => ({ default: m.ChatWidget })), {
-  ssr: false,
-});
